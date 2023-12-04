c042dfa7b4702e8f939bdb91e637eb1d2fda0a56
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2023-12-04 20:55:20 -0300

    add endpoints description

--- src/main/java/dev/vanderloureiro/springrobotjson/robot/PositionController.java
+++ src/main/java/dev/vanderloureiro/springrobotjson/robot/PositionController.java
@@ -2,6 +2,7 @@ package dev.vanderloureiro.springrobotjson.robot;
 
 import dev.vanderloureiro.springrobotjson.robot.io.Moviment;
 import dev.vanderloureiro.springrobotjson.robot.io.Position;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +19,13 @@ public class PositionController {
     private GetCurrentPositionService getCurrentPositionService;
 
     @GetMapping
+    @Operation(summary = "Busca posição", description = "Busca posição atual do robô")
     public ResponseEntity<Position> get() {
         return ResponseEntity.ok(this.getCurrentPositionService.execute());
     }
 
     @PostMapping
+    @Operation(summary = "Realiza movimento", description = "Realiza movimentos do robô de rotação ou seguir em frente")
     public ResponseEntity<Position> post(@RequestBody Moviment input) {
         return ResponseEntity.ok(this.moveRobotService.execute(input));
     }
